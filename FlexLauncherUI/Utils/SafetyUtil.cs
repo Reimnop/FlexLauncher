1c8ed45957c4cb9ed069cff728e9a1cf50bf1133
Reimnop <rayngu2005@gmail.com>
2023-08-18 07:03:16 +0700

    Small work

---
+++ FlexLauncherUI/Utils/SafetyUtil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FlexLauncherUI.Utils;
+
+public static class SafetyUtil
+{
+    public static T EnsureNotNull<T>(this T? obj, string name) where T : class
+    {
+        if (obj is null)
+            throw new ArgumentNullException(name);
+
+        return obj;
+    }
+}
